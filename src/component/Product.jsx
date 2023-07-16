30169552c14a648e5fb8b351ea24e414e90ce43b
developerpyk <developerpyk@gmail.com>
2023-07-16 13:45:23 +0900

    refactor: Header, Dropdown 컴포넌트 [#2]

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './product.css';
 
 export default function Product() {
--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {useEffect, useState} from 'react';
 import Product from '../component/Product';
 import DebugComponent from '../util/DebugComponent';
